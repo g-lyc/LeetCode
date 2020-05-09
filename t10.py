44763f70b575eb11b7e7f107695ecde2a0904722
godLYC <309080979@qq.com>
2020-05-09 16:35:06 +0800

    t10

--- t10.py
+++ t10.py
@@ -48,9 +48,47 @@ import os, sys
 # 输出: false
 
 
+# 递归解法
 class Solution:
     def isMatch(self, s: str, p: str) -> bool:
-        pass
+        if not p:
+            return not s
+
+        first_match = bool(s) and p[0] in {s[0], '.'}
+
+        if len(p) >= 2 and p[1]=="*":
+            return self.isMatch(s, p[2:]) or first_match and self.isMatch(s[1:], p)
+        else:
+            return first_match and self.isMatch(s[1:], p[1:])
+
+
+# 动态规划解法
+class Solution:
+    def isMatch(self, s: str, p: str) -> bool:
+        memo = {}
+        def dp(i, j):
+            if (i, j) not in memo:
+                if j == len(p):
+                    return i == len(s)
+
+                else:
+                    first_match = i < len(s) and p[j] in {s[i], '.'}
+                    if j + 1 < len(p) and p[j+1] == '*':
+                        ans = dp(i, j+2) or first_match and dp(i+1, j)
+                    else:
+                        ans = first_match and dp(i+1, j+1)
+
+                memo[(i, j)] = ans
+
+            return memo[(i, j)]
+
+        return dp(0, 0)
 
 
 
+result = Solution()
+
+s = "mississippi"
+p = "mis*is*p*."
+
+print(result.isMatch(s,p))
