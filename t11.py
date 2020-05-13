3fac49f854ae0391413d46e536c49b0cd912b71e
godLYC <309080979@qq.com>
2020-05-13 17:21:32 +0800

    t11

---
+++ t11.py
@@ -0,0 +1,50 @@
+# -*- conding:utf-8 -*-
+#Author:lyc
+import os, sys
+
+
+# 给你 n 个非负整数 a1，a2，...，an，每个数代表坐标中的一个点 (i, ai) 。在坐标内画 n 条垂直线，垂直线 i 的
+# 两个端点分别为 (i, ai) 和 (i, 0)。找出其中的两条线，使得它们与 x 轴共同构成的容器可以容纳最多的水。
+#
+# 说明：你不能倾斜容器，且 n 的值至少为 2。
+
+
+# 解法1 双端指针
+class Solution:
+    def maxArea(self, height) -> int:
+        l = 0
+        r = len(height) - 1
+        contain = 0
+        while l < r:
+            contain = max(contain, (r - l) * min(height[l], height[r]))
+            if height[l] > height[r]:
+                r -= 1
+            else:
+                l += 1
+
+        return contain
+
+# 解法2 暴力穷举（时间会超出限制）
+class Solution:
+    def maxArea(self, height) -> int:
+
+        max_area = []
+
+        if len(height) == 2:
+            area = min(height)
+            return area
+        else:
+            for i in range(len(height)-1):
+                for j in range(len(height[i+1:])):
+                    area = min([height[i], height[i+1:][j]]) * (j+1)
+                    max_area.append(area)
+
+        return max(max_area)
+
+
+
+result = Solution()
+#x = [1,8,6,2,5,4,8,3,7]
+x = [1,2,4,3]
+print(result.maxArea(x))
+
